ef614db3e0ecb2847c51166fed243cffb59b74f3
Nicolas Lazarte <nicolas.lazarte@fractale.vision>
2024-04-09 11:17:28 -0400

    Implémentation du calcul de la logique pour le IMC

--- src/App.js
+++ src/App.js
@@ -1,25 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import ChampSaisie from './components/ChampSaisie';
+import CalculerImc from './components/CalculerImc';
+import AfficherResultat from './components/AfficherResultat';
 
-function App() {
+const App = () => {
+  const [poids, setPoids] = useState(0);
+  const [taille, setTaille] = useState(0);
+  const [imc, setImc] = useState(0);
+
+  const calculerIMC = () => {
+    const tailleEnMetres = taille / 100;
+    const valeurIMC = poids / (tailleEnMetres * tailleEnMetres);
+    setImc(valeurIMC);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Calculateur de IMC</h1>
+      <ChampSaisie label="Poids (kg)" value={poids} onChange={setPoids} />
+      <ChampSaisie label="Taille (cm)" value={taille} onChange={setTaille} />
+      <CalculerImc onClick={calculerIMC} />
+      {imc > 0 && <AfficherResultat imc={imc} />}
     </div>
   );
-}
+};
 
 export default App;
